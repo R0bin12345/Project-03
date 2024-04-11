ea42255b437de089c8807d01d1eebdc6449a42f2
R0bin12345 <144021137+R0bin12345@users.noreply.github.com>
2024-04-11 12:10:45 +0200

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Databaseverbinding
-try {
-    $db = new PDO("mysql:host=localhost;dbname=webshop", "root", "");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbindingsfout: " . $e->getMessage());
-}
-?>
